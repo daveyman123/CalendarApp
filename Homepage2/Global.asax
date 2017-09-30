9e031416c9cc7797726cb529f187e629e0541e58
david graff <djgraff1@cougars.ccis.edu>
2017-09-30 12:15:01 -0700

    Add project files.

---
+++ Homepage2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Homepage2.MvcApplication" Language="C#" %>
---
+++ Homepage2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Homepage2.Startup))]
+namespace Homepage2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
